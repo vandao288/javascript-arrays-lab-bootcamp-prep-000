f94493352abd3def7202320089c312b081904a87
Van A. Dao <daoanhvan81@gmail.com>
2017-12-17 22:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const app = "I don't do much.";
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  
+  var newArray = kittens.appendKitten(name);
+  return newArray;
 }
+
+function 
