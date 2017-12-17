1d18824f4c8a1a0736590f213957a65fc56df59a
Van A. Dao <daoanhvan81@gmail.com>
2017-12-17 22:27:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const app = "I don't do much.";
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name) {
-  
+  kittens.push(name);
 }
 
 function destructivelyPrependKitten(name) {
-  
+  kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten() {
-  
+  kittens.pop();
 }
+
+function 
